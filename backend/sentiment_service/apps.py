e71bb540b98ff3ba516f24bc6cca3fe7043190c4
nardos627 <nardoshaddis5@gmail.com>
2024-05-20 13:11:39 +0300

    backend version 1

--- backend/sentiment_service/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SentimentServiceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "sentiment_service"
--- backend/sentiment_service/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-class Review(models.Model):
-         user = models.ForeignKey('user_management.User', related_name='reviews', on_delete=models.CASCADE)
-         text = models.TextField()
-         rating = models.IntegerField()  # You can adjust the field based on your rating system
-         created_at = models.DateTimeField(auto_now_add=True)
-         # Add other fields as needed for sentiment analysis
