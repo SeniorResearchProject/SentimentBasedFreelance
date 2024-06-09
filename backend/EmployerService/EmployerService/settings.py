e637496511593a9751640e4ec645dd6ddbc6fda9
Nardos Hadis <88848883+nardos627@users.noreply.github.com>
2024-06-09 22:54:17 +0300

    Update settings.py

--- backend/EmployerService/EmployerService/settings.py
+++ backend/EmployerService/EmployerService/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
      "job_posting_app",
      'corsheaders',
+     'job_posting_app.middleware.CorsMiddleware',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
