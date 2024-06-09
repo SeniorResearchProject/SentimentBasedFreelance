35359cc48197c93dc40240cde4e112443228b111
Nardos Hadis <88848883+nardos627@users.noreply.github.com>
2024-06-09 23:20:47 +0300

    Update middleware.py

--- backend/EmployerService/job_posting_app/middleware.py
+++ backend/EmployerService/job_posting_app/middleware.py
@@ -2,5 +2,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 class CorsMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        response["Access-Control-Allow-Origin"] = "*"  # You can specify a domain instead of '*'
+        response["Access-Control-Allow-Origin"] = "*"  # Allow requests from any origin
+        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"  # Allow specific HTTP methods
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"  # Allow specific headers
         return response
