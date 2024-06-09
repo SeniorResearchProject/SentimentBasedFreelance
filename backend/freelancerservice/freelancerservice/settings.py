4018a67330c0ae64a14845e4e5a1d405733c2754
Nardos Hadis <88848883+nardos627@users.noreply.github.com>
2024-06-09 22:18:29 +0300

    Update settings.py

--- backend/freelancerservice/freelancerservice/settings.py
+++ backend/freelancerservice/freelancerservice/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3001',
+      'http://localhost:3000',
+    # Add more origins if needed
+]
 # Application definition
 
 INSTALLED_APPS = [
